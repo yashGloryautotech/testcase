79f233a6bea67b5fcc5ae2a48a691058963756d3
yash balkhande <yash.gloryautotech@gmail.com>
2020-12-10 17:44:30 +0530

    first commit

--- test/testindex.js
+++ test/testindex.js
@@ -3,7 +3,7 @@ const app=require('../index')
 
 describe('index',function(){
     it('add number should be 2', function(){
-        let result=app.addNumbers(2,1)
+        let result=app.addNumbers(1,1)
         assert.equal(result,2)
     }); 
      
