b70ce3a7f8511115159c2681fda44c47e237ad5e
yash balkhande <yash.gloryautotech@gmail.com>
2020-12-10 15:44:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports={
+    addNumbers:function(a,b)
+    {
+        return a+b;
+    },
+    sayHello:function(){
+        return "Hello"
+    } 
+}
---
+++ test/testindex.js
@@ -0,0 +1,21 @@
+const assert  = require('chai').assert;
+const app=require('../index')
+
+describe('index',function(){
+    it('add number should be 2', function(){
+        let result=app.addNumbers(4,4)
+        assert.equal(result,2)
+    }); 
+     
+    it('hello is typeof string',function(){
+        assert.typeOf(app.sayHello(),'string')
+    });
+    
+    it('return Hello',function(){
+        assert.equal(app.sayHello(),'Hello')
+    });
+    
+    it('add number should return number',function(){
+        assert.typeOf(app.addNumbers(),'number')
+    }); 
+});
